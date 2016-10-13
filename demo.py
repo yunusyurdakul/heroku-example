3b76346de4ff01ab691ec9736f64bf62e158a888
yunusyurdakul <ycagriyurdakul@gmail.com>
2016-10-13 22:31:19 +0300

    Updated the demo site

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ from flask import request
 from algorithm import _findElementsBetween
 
 import yaml
+import string
 
 app = Flask(__name__)
 
@@ -22,14 +23,18 @@ def compute():
 	else:
 		input1 = request.form['input1']
 		app.logger.debug(input1)
-	
 		print 'input1: ' + input1
 
 		input2 = request.form['input2']
 		app.logger.debug(input2)
 		print 'input2: ' + input2
 
-		result = _findElementsBetween(int(input1), int(input2), [10,'yunus',2,3,4,5,6])
+		input3 = request.form['input3']
+		input3 =  [str(x) for x in input3.splitlines()]
+		app.logger.debug(input3)
+		print 'input3: ' + str(input3)
+
+		result = _findElementsBetween(int(input1), int(input2), input3)
 		print result
 
 		return render_template('compute.html', result=result)
